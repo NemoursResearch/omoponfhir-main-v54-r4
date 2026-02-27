ef675c85ace386cec246f6138ef3868189c04b92
Bill Moyers <billmoyers@fastmail.com>
2026-02-27 09:45:46 -0500

    Enhancement: Add omoponfhir-smart-bulk-data-server submodule.

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -1,3 +1,4 @@
 git submodule update --remote -- omoponfhir-omopv5-sql
 git submodule update --remote -- omoponfhir-omopv5-r4-mapping
 git submodule update --remote -- omoponfhir-r4-server
+git submodule update --remote -- omoponfhir-smart-bulk-data-server
